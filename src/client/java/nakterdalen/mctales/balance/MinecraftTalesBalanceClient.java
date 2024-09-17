8440aa23ac9f6adc1a51d0f6152fccd1679d0603
Nakterdalen <nakterdalen@gmail.com>
2024-09-17 16:15:41 +0200

    Add files via upload

---
+++ src/client/java/nakterdalen/mctales/balance/MinecraftTalesBalanceClient.java
@@ -0,0 +1,10 @@
+package nakterdalen.mctales.balance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftTalesBalanceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
