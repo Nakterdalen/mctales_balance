30004a8eb1c32ebaa7cf27011fcd2fea62965c38
Nakterdalen <nakterdalen@gmail.com>
2024-11-14 20:51:10 +0100

    Add enchantment penalty for Mending repairs.

---
+++ src/main/java/nakterdalen/mctales/balance/mixin/enchantingmixins/MendingMixin.java
@@ -0,0 +1,25 @@
+package nakterdalen.mctales.balance.mixin.enchantingmixins;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EnchantmentHelper.class)
+public abstract class MendingMixin {
+
+    @Inject(method = "getRepairWithXp", at = @At(value = "RETURN"), cancellable = true)
+    private static void reduceMendingRepair(ServerWorld world, ItemStack stack, int baseRepairWithXp, CallbackInfoReturnable<Integer> cir) {
+        int level = 0;
+        for (RegistryEntry<Enchantment> entry : EnchantmentHelper.getEnchantments(stack).getEnchantments()) {
+            level += EnchantmentHelper.getLevel(entry, stack);
+        }
+        cir.setReturnValue(cir.getReturnValue()/(Math.max(1, level)));
+    }
+
+}
