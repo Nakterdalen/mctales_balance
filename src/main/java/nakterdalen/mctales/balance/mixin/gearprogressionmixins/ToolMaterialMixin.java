0f108bd32fd2b3707e13ff635c4d778382699bda
Nakterdalen <nakterdalen@gmail.com>
2024-11-20 18:25:22 +0100

    Cleanup

--- src/main/java/nakterdalen/mctales/balance/mixin/gearprogressionmixins/ToolMaterialMixin.java
+++ src/main/java/nakterdalen/mctales/balance/mixin/gearprogressionmixins/ToolMaterialMixin.java
@@ -7,9 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.tag.TagKey;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ToolMaterial.class)
