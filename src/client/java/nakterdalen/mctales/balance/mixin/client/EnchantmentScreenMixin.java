17e0499327dad5bf66c170efd2e55e371031d86b
Nakterdalen <nakterdalen@gmail.com>
2025-10-21 21:32:34 +0200

    Write initial EnchantmentScreen Mixin.

--- src/client/java/nakterdalen/mctales/balance/mixin/client/EnchantmentScreenMixin.java
+++ src/client/java/nakterdalen/mctales/balance/mixin/client/EnchantmentScreenMixin.java
@@ -1,15 +1,18 @@
 package nakterdalen.mctales.balance.mixin.client;
 
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.EnchantingPhrases;
 import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.EnchantmentScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,21 +24,30 @@ import java.util.Objects;
 public abstract class EnchantmentScreenMixin extends HandledScreen<EnchantmentScreenHandler> {
 
     //@Shadow unique only temp
-    @Unique
-    private void drawBook(DrawContext context, int x, int y, float delta) {}
+    @Final
+    @Shadow
+    protected abstract void drawBook(DrawContext context, int x, int y);
 
-    @Unique
-    private static final Identifier[] LEVEL_TEXTURES = new Identifier[]{Identifier.ofVanilla("container/enchanting_table/level_1"), Identifier.ofVanilla("container/enchanting_table/level_2"), Identifier.ofVanilla("container/enchanting_table/level_3")};
-    @Unique
-    private static final Identifier[] LEVEL_DISABLED_TEXTURES = new Identifier[]{Identifier.ofVanilla("container/enchanting_table/level_1_disabled"), Identifier.ofVanilla("container/enchanting_table/level_2_disabled"), Identifier.ofVanilla("container/enchanting_table/level_3_disabled")};
-    @Unique
-    private static final Identifier ENCHANTMENT_SLOT_DISABLED_TEXTURE = Identifier.ofVanilla("container/enchanting_table/enchantment_slot_disabled");
-    @Unique
-    private static final Identifier ENCHANTMENT_SLOT_HIGHLIGHTED_TEXTURE = Identifier.ofVanilla("container/enchanting_table/enchantment_slot_highlighted");
-    @Unique
-    private static final Identifier ENCHANTMENT_SLOT_TEXTURE = Identifier.ofVanilla("container/enchanting_table/enchantment_slot");
-    @Unique
-    private static final Identifier TEXTURE = Identifier.ofVanilla("textures/gui/container/enchanting_table.png");
+    @Final
+    @Shadow
+    private static Identifier[] LEVEL_TEXTURES;
+    @Final
+    @Shadow
+    private static Identifier[] LEVEL_DISABLED_TEXTURES;
+    @Final
+    @Shadow
+    private static Identifier ENCHANTMENT_SLOT_DISABLED_TEXTURE;
+    @Final
+    @Shadow
+    private static Identifier ENCHANTMENT_SLOT_HIGHLIGHTED_TEXTURE;
+    @Final
+    @Shadow
+    private static Identifier ENCHANTMENT_SLOT_TEXTURE;
+    @Final
+    @Shadow
+    private static Identifier TEXTURE;
+
+    //Identifier.ofVanilla("textures/gui/container/enchanting_table.png");
 
     public EnchantmentScreenMixin(ScreenHandler handler, PlayerInventory inventory, Text title) {
         super((EnchantmentScreenHandler) handler, inventory, title);
@@ -43,54 +55,54 @@ public abstract class EnchantmentScreenMixin extends HandledScreen<EnchantmentSc
 
     @Inject(method = "drawBackground", at = @At("HEAD"), cancellable = true)
     private void newBackground(DrawContext context, float delta, int mouseX, int mouseY, CallbackInfo ci) {
+        //copied
         int i = (this.width - this.backgroundWidth) / 2;
-
         int j = (this.height - this.backgroundHeight) / 2;
-        //context.drawTexture(RenderPipeline::getGuiTextured, TEXTURE, i, j, 0.0F, 0.0F, this.backgroundWidth, this.backgroundHeight, 256, 256);
-        this.drawBook(context, i, j, delta);
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, i, j, 0.0F, 0.0F, this.backgroundWidth, this.backgroundHeight, 256, 256);
+
+        this.drawBook(context, i, j);
         EnchantingPhrases.getInstance().setSeed(this.handler.getSeed());
+        this.drawBook(context, i, j);
         int lapisCount = this.handler.getLapisCount();
-        //int enchantability = Objects.requireNonNull(this.handler.slots.getFirst().getStack().get(DataComponentTypes.ENCHANTABLE)).value();
 
-        //for (int alt = 0; alt < enchantability; alt++) {
+        int enchantability = this.handler.slots.getFirst().getStack().isEmpty() ? 0 : Objects.requireNonNull(this.handler.slots.getFirst().getStack().get(DataComponentTypes.ENCHANTABLE)).value();
 
-        //}
-
-        for(int l = 0; l < 3; ++l) {
+        for(int alt = 0; alt < enchantability; alt++) {
             int m = i + 60;
             int n = m + 20;
-            int xpLevel = this.handler.enchantmentPower[l];
-            if (xpLevel == 0) {
-                //context.drawGuiTexture(RenderLayer::getGuiTextured, ENCHANTMENT_SLOT_DISABLED_TEXTURE, m, j + 14 + 19 * l, 108, 19);
-            } else {
-                String string = "" + xpLevel;
-                int p = 86 - this.textRenderer.getWidth(string);
-                StringVisitable stringVisitable = EnchantingPhrases.getInstance().generatePhrase(this.textRenderer, p);
-                int q = 6839882;
-                if ((lapisCount < l + 1 || Objects.requireNonNull(Objects.requireNonNull(this.client).player).experienceLevel < xpLevel) && !Objects.requireNonNull(Objects.requireNonNull(this.client).player).getAbilities().creativeMode) {
-                    //context.drawGuiTexture(RenderLayer::getGuiTextured, ENCHANTMENT_SLOT_DISABLED_TEXTURE, m, j + 14 + 19 * l, 108, 19);
-                    //context.drawGuiTexture(RenderLayer::getGuiTextured, LEVEL_DISABLED_TEXTURES[l], m + 1, j + 15 + 19 * l, 16, 16);
-                    //context.drawTextWrapped(this.textRenderer, stringVisitable, n, j + 16 + 19 * l, p, (q & 16711422) >> 1);
-                    q = 4226832;
-                } else {
-                    int r = mouseX - (i + 60);
-                    int s = mouseY - (j + 14 + 19 * l);
-                    if (r >= 0 && s >= 0 && r < 108 && s < 19) {
-                        //context.drawGuiTexture(RenderLayer::getGuiTextured, ENCHANTMENT_SLOT_HIGHLIGHTED_TEXTURE, m, j + 14 + 19 * l, 108, 19);
-                        q = 16777088;
-                    } else {
-                        //context.drawGuiTexture(RenderLayer::getGuiTextured, ENCHANTMENT_SLOT_TEXTURE, m, j + 14 + 19 * l, 108, 19);
-                    }
 
-                    //context.drawGuiTexture(RenderLayer::getGuiTextured, LEVEL_TEXTURES[l], m + 1, j + 15 + 19 * l, 16, 16);
-                    //context.drawTextWrapped(this.textRenderer, stringVisitable, n, j + 16 + 19 * l, p, q);
-                    q = 8453920;
+            int xpLevel = alt + 1;
+
+            String string = "" + xpLevel;
+            int p = 86 - this.textRenderer.getWidth(string);
+            StringVisitable stringVisitable = EnchantingPhrases.getInstance().generatePhrase(this.textRenderer, p);
+
+            int q = -9937334;
+            if ((lapisCount < alt + 1 || Objects.requireNonNull(Objects.requireNonNull(this.client).player).experienceLevel < xpLevel) && !Objects.requireNonNull(Objects.requireNonNull(this.client).player).getAbilities().creativeMode) {
+                //alternative not available
+                context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, ENCHANTMENT_SLOT_DISABLED_TEXTURE, m, j + 14 + 19 * alt, 108, 19);
+                context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, LEVEL_DISABLED_TEXTURES[Math.min(alt, 2)], m + 1, j + 15 + 19 * alt, 16, 16);
+                context.drawWrappedText(this.textRenderer, stringVisitable, n, j + 16 + 19 * alt, p, ColorHelper.fullAlpha((q & 16711422) >> 1), false);
+                q = -12550384;
+            } else {
+                //alternative available
+                int r = mouseX - (i + 60);
+                int s = mouseY - (j + 14 + 19 * alt);
+
+                if (r >= 0 && s >= 0 && r < 108 && s < 19) {
+                    context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, ENCHANTMENT_SLOT_HIGHLIGHTED_TEXTURE, m, j + 14 + 19 * alt, 108, 19);
+                    q = 16777088;
+                } else {
+                    context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, ENCHANTMENT_SLOT_TEXTURE, m, j + 14 + 19 * alt, 108, 19);
                 }
 
-                context.drawTextWithShadow(this.textRenderer, string, n + 86 - this.textRenderer.getWidth(string), j + 16 + 19 * l + 7, q);
+                context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, LEVEL_TEXTURES[Math.min(alt, 2)], m + 1, j + 15 + 19 * alt, 16, 16);
+                context.drawWrappedText(this.textRenderer, stringVisitable, n, j + 16 + 19 * alt, p, q, false);
+                q = 8453920;
             }
+
+            context.drawTextWithShadow(this.textRenderer, string, n + 86 - this.textRenderer.getWidth(string), j + 16 + 19 * alt + 7, q);
         }
         ci.cancel();
     }
-
 }
